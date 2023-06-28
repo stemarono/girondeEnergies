b99102cc7df325b7bb956f10402c242b47173ca8
stemarono <stephanielacaze93@gmail.com>
2023-06-28 09:45:01 +0200

    mise en pace de l'accueil et création de la page qui sommes-nous

---
+++ src/Controller/PresentationController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PresentationController extends AbstractController
+{
+    #[Route('/presentation', name: 'app_presentation')]
+    public function index(): Response
+    {
+        return $this->render('presentation/index.html.twig', [
+            'controller_name' => 'PresentationController',
+        ]);
+    }
+    #[Route('/presentation/qui_sommes_nous', name: 'app_about')]
+    public function about(): Response
+    {
+        return $this->render('presentation/about.html.twig');
+    }
+}
